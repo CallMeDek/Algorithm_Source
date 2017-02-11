85c2d7b9322a86cb848318b3e5557fcf1fa9b1db
Dekgyu Cheo <choidaegyu9671@gmail.com>
2017-02-11 22:34:10 +0900

    Add / I made two classes

---
+++ testSolution_C++/testSolution_C++/combination/Combination.h
@@ -0,0 +1,24 @@
+#ifndef COMBINATION_HEADER
+#define COMBINATION_HEADER
+
+/*
+Creator: Austine Cheo(Nick name - Dek)
+Creation date: 02 / 11 / 2017
+Last revision : 02 / 11 / 2017
+Revision contents:
+1. Creation of this file
+*/
+
+class combination {
+public:
+	combination();
+	virtual ~combination();
+};
+
+//We will calculate the combination using this.
+class permutation {
+	permutation();
+	virtual ~permutation();
+};
+
+#endif
