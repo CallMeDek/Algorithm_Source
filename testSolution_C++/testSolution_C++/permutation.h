559dc96ef62b9d438f2d740c25eae65e283b600e
Dekgyu Cheo <choidaegyu9671@gmail.com>
2017-02-11 19:16:08 +0900

    Rev / Constructing the basie structure of permutation.h

--- testSolution_C++/testSolution_C++/permutation.h
+++ testSolution_C++/testSolution_C++/permutation.h
@@ -1,1 +1,24 @@
+#ifndef PERMUTATION_HEADER
+#define PERMUTATION_HEADER
 
+/*
+Creator: Austine Cheo(Nick name - Dek)
+Creation date: 02 / 11 / 2017
+Last revision : 02 / 11 / 2017
+Revision contents:
+1. Creation of this file
+*/
+
+class Permutation {
+
+public:
+	Permutation();
+	virtual ~Permutation();
+
+
+
+private:
+
+};
+
+#endif
