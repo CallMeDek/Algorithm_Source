ae2d81803f6c11f9778d997fb93d01d81f844024
Dekgyu Cheo <choidaegyu9671@gmail.com>
2017-02-14 18:43:28 +0900

    Setting before implementation

--- testSolution_C++/testSolution_C++/test.cpp
+++ testSolution_C++/testSolution_C++/test.cpp
@@ -1,22 +1,7 @@
 #include <iostream>
-#include "userInput.h"
-#include "fastSum.h"
 
 int main(void)
 {
-	TypedInput a;
-	a.setSize();
-	//a.testCode();
-
-	FiledInput b;
-	b.setSize();
-	//b.testCode();
-
-	FastSum c(a.getSize());
-	c.execute();
-
-	FastSum d(b.getSize());
-	d.execute();
 
 	return 0;
 }
