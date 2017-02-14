2b2e2bc378ba554acd1294cface2a0d6e723ac8e
Dekgyu Cheo <choidaegyu9671@gmail.com>
2017-02-14 19:42:50 +0900

    Add / Setting the basic of Matrix class

--- testSolution_C++/testSolution_C++/matrix_product/matrix.h
+++ testSolution_C++/testSolution_C++/matrix_product/matrix.h
@@ -1,8 +1,25 @@
 #ifndef MATRIX_HEADER
 #define MATRIX_HEADER
 
-class Matrix {
+/*
+Creator: Austine Cheo(Nick name - Dek)
+Creation date: 02 / 14 / 2017
+Last revision : 02 / 14 / 2017
+Revision contents:
+1. Creation of this file
+*/
 
+class Matrix {
+public:
+	Matrix();
+	virtual ~Matrix();
+
+	//After getting size from user, Make an two-dimentional array
+	void make2DArray();
+
+protected:
+	int size;
+	int** _array;
 };
 
 #endif
