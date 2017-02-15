a293a2c7091d7525bba4583dc749f2d81cbba562
Dekgyu Cheo <choidaegyu9671@gmail.com>
2017-02-15 20:48:10 +0900

    Creating a new solution

---
+++ testSolution_C/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Creator: Austine Cheo(Nick name - Dek)
+Creation date: 02 / 11 / 2017
+Last revision : 02 / 11 / 2017
+Revision contents:
+1. Creation of this file
+*/
+
+//This solution is created for implementing involution of matrix
+
+int main(void)
+{
+
+}
